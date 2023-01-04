e95cd6fd55666fa1596bce0fbc8a2ad2d373a7f9
supanida-p41 <supanida.p@betimes.biz>
2023-01-04 16:53:14 +0700

    commit04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit,ViewChild } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { Router } from '@angular/router';
+import { NgConfirmService } from 'ng-confirm-box';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online-store';
+  displayedColumns: string[] = ['productName', 'category','date','product', 'price', 'comment','action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor(private dialog : MatDialog, private api : ApiService, private confirmService: NgConfirmService,private router : Router  ){
+
+  }
+ 
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width : '50%'
+    }).afterClosed().subscribe(val=>{
+      if(val =='save'){
+        this.getAllProducts();
+      }
+    })
+  }
+  getAllProducts(){
+    this.api.getProduct()
+    .subscribe({
+      next:(res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error:(err)=>{
+        alert("Error while fetching the Records!")
+      }
+    })
+  }
+
+  editProduct(row : any){
+    this.dialog.open(DialogComponent,{
+      width:'30%',
+      data: row
+    }).afterClosed().subscribe(val=>{
+      if(val=='update'){
+        this.getAllProducts();
+      }
+    })
+  }
+  
+
+  deleteProduct(id:number){
+   this.confirmService.showConfirm("Are you sure?",()=>{
+    this.api.deleteProduct(id)
+    .subscribe({
+      next:(res)=>{
+        //alert("Deleted Successfully!");
+        this.getAllProducts();
+      },
+      error:()=>{
+        alert("Error while deleting the product!")
+      }
+    })
+   },
+   ()=>{
+      alert("Selected No")
+   }
+   )
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
