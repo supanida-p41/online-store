8cc416c4e7e42cc6bee2ab845655448973d02f5a
supanida-p41 <supanida.p@betimes.biz>
2023-01-17 16:41:15 +0700

    afternoon commit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
+    public signupForm !: FormGroup;
+    constructor(private formBuilder : FormBuilder) {}
+
+    ngOnInit(): void {
+      this.signupForm = this.formBuilder.group({
+        fullname:[''],
+        email:[''],
+        password:[''],
+        mobile:['']
+      })
+    }
 }
