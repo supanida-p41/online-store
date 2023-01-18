0ae51a0998892fa9248402b412e99538ac5fefa0
supanida-p41 <supanida.p@betimes.biz>
2023-01-18 14:23:24 +0700

    loginlogout update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  public loginForm!: FormGroup
+  constructor(private formBuilder : FormBuilder,private http : HttpClient,private router:Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email:[''],
+      password:['']
+    })
+  }
+  login(){
+    this.http.get<any>("http://localhost:3000/signupUsers")
+    .subscribe(res=>{
+      const user = res.find((a:any)=>{
+        return a.mail === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+      if(user){
+        alert("Login Success");
+        this.loginForm.reset();
+        this.router.navigate(['dashboard'])
+      }else{
+        alert("user not found");
+      }
+    },err=>{
+      alert("Wrong!")
+    })
+  }
 }
